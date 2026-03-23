9bbbbf7372ad773271a08fe947ccce9cec32d7e5
Muhammad <zuraizmuhammad143@gmail.com>
2026-03-24 03:24:17 +0500

    Trusting proxy to allow railway

--- NUST-helpdesk-api/src/server.js
+++ NUST-helpdesk-api/src/server.js
@@ -15,6 +15,7 @@ const usersRoutes = require('./routes/users.routes');
 const notificationsRoutes = require('./routes/notifications.routes');
 
 const app = express();
+app.set('trust proxy', 1); 
 app.use(cors());
 app.use(express.json());
 app.use(generalLimiter);
